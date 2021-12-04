99101ffb6f093360c715bfcd4f40b57e98f15166
Dmitriy Metelkov <d.metelkov@mail.ru>
2021-12-05 02:39:14 +0300

    add CardWeather component

---
+++ src/components/CardWeather.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="card-weather">
+    <h3 class="card-weather__name">
+      {{ weather.city.name }}
+    </h3>
+    <p class="card-weather__location">
+      {{ weather.country.name.common }}
+    </p>
+    <div class="card-weather__params">
+      <div
+          v-for="(value, title) in params"
+          :key="title"
+          class="card-weather__params-row"
+      >
+        <div class="card-weather__params-cell">{{ title }}</div>
+        <div class="card-weather__params-cell">{{ value }}</div>
+      </div>
+      <p class="card-weather__updated_date">
+        {{ getTimeAgo(weather.updatedDate) }}
+      </p>
+    </div>
+    <div class="card-weather__actions">
+      <app-button
+          class="card-weather__action"
+          type="primary"
+          @click="$emit('reload')"
+      >
+        Reload
+      </app-button>
+      <app-button
+          v-if="removable"
+          class="card-weather__action"
+          type="primary"
+          @click="$emit('remove')"
+      >
+        Remove
+      </app-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getTimeAgo } from '@/utils/formatDate'
+import AppButton from '@/components/ui/AppButton.vue'
+
+export default {
+  name: 'CardWeather',
+  components: {
+    AppButton
+  },
+  props: {
+    weather: {
+      type: Object,
+      required: true
+    },
+    removable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    params() {
+      const { weather, main } = this.weather.city
+      return {
+        Weather: weather[0].main,
+        Temperature: `${main.temp} ℃`,
+        Humidity: `${main.humidity} %`
+      }
+    }
+  },
+  methods: {
+    getTimeAgo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-weather {
+  width: 350px;
+  padding: 24px;
+  box-shadow: 0 2px 10px rgba(10, 10, 10, 0.25);
+  border-radius: 6px;
+}
+
+.card-weather__name {
+  margin-bottom: 15px;
+  font-size: 32px;
+  line-height: 38px;
+  font-weight: 700;
+}
+
+.card-weather__location {
+  margin-bottom: 40px;
+  font-size: 18px;
+  line-height: 24px;
+}
+
+.card-weather__actions {
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+}
+
+.card-weather__params {
+  margin-bottom: 8px;
+}
+
+.card-weather__params-row {
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 12px;
+  border-bottom: 1px solid var(--color-border);
+
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+}
+
+.card-weather__params-cell {
+  font-size: 18px;
+  line-height: 24px;
+}
+
+.card-weather__updated_date {
+  margin-bottom: 32px;
+  text-align: right;
+  font-size: 16px;
+  line-height: 24px;
+  color: var(--color-text-secondary);
+}
+</style>
