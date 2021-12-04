220652b99666e7245c27857c27c9688fab23244b
Dmitriy Metelkov <d.metelkov@mail.ru>
2021-12-04 03:12:16 +0300

    add ModalChooseCity initial template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
-  <div></div>
+  <ModalChooseCity
+    v-if="chooseCityModalIsVisible"
+    @close="chooseCityModalIsVisible = false"
+  />
+  <AppButton
+    class="add-btn"
+    icon="plus"
+    @click="chooseCityModalIsVisible = true"
+  >
+  </AppButton>
 </template>
 
 <script>
+import AppButton from '@/components/ui/AppButton.vue'
+import ModalChooseCity from '@/components/ModalChooseCity.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppButton,
+    ModalChooseCity
+  },
+  data() {
+    return {
+      chooseCityModalIsVisible: false
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.add-btn {
+  position: fixed;
+  right: 45px;
+  bottom: 34px;
+}
+</style>
---
+++ src/components/ModalChooseCity.vue
@@ -0,0 +1,84 @@
+<template>
+  <AppModal
+      class="modal-choose-city"
+      title="Choose a city"
+      description="To find city start typing and pick one from the suggestions"
+  >
+    <AppInput
+      class="modal-choose-city__input"
+      v-model="city"
+      placeholder="Search city"
+    />
+    <div class="modal-choose-city__actions">
+      <AppButton
+        class="modal-choose-city__action"
+        @click="clear"
+      >
+        Clear
+      </AppButton>
+      <AppButton
+        class="modal-choose-city__action"
+        type="primary"
+        @click="$emit('close')"
+      >
+        Cancel
+      </AppButton>
+      <AppButton class="modal-choose-city__action">
+        Add
+      </AppButton>
+    </div>
+  </AppModal>
+</template>
+
+<script>
+import AppModal from '@/components/ui/AppModal.vue'
+import AppInput from '@/components/ui/AppInput.vue'
+import AppButton from '@/components/ui/AppButton.vue'
+
+export default {
+  name: 'ModalChooseCity',
+  emits: ['close'],
+  components: {
+    AppModal,
+    AppInput,
+    AppButton
+  },
+  data() {
+    return {
+      city: ''
+    }
+  },
+  methods: {
+    clear() {
+      this.city = ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+::v-global(.modal-choose-city) {
+  width: 751px;
+}
+
+.modal-choose-city__input {
+  margin-top: 51px;
+  width: 100%;
+  margin-bottom: 140px;
+}
+
+.modal-choose-city__actions {
+  display: flex;
+  align-items: center;
+}
+
+.modal-choose-city__action {
+  &:nth-child(2) {
+    margin-left: auto;
+  }
+
+  &:not(:last-child) {
+    margin-right: 31px;
+  }
+}
+</style>
