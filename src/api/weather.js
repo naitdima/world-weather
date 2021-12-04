a4b44e24e9d95ec2bab9ec0cd472f83162d2537d
Dmitriy Metelkov <d.metelkov@mail.ru>
2021-12-05 01:50:13 +0300

    change units for weather api

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,16 +1,15 @@
 import { NetworkError } from '@/api/network'
+import Base from '@/api/base'
 const { VUE_APP_OPENWEATHERMAP_API_KEY } = process.env
 
 const ERROR_MESSAGE = {
     401: 'Check that the API-key is correct'
 }
 
-import Base from '@/api/base'
-
 class Weather extends Base {
     async getByCity(city) {
         try {
-            return await this._api.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${VUE_APP_OPENWEATHERMAP_API_KEY}`)
+            return await this._api.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${VUE_APP_OPENWEATHERMAP_API_KEY}&units=metric`)
         } catch (e) {
             const errorMessage = ERROR_MESSAGE[e.code]
             if (errorMessage) {
