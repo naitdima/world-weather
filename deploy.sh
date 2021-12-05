5c721c5babb0f8d87e3a57918b8416f6475e543e
Dmitriy Metelkov <d.metelkov@mail.ru>
2021-12-05 23:19:31 +0300

    deploy fixes

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:naitdima/world-weather.git main:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/<REPO>/'
+    ? '/world-weather/'
     : '/'
   chainWebpack: config => {
     config.module
